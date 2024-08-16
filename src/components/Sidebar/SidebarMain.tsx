5e0f1b249dc91d1bbca9ff1848d954399357d51e
suman mishra <suman132j@gmail.com>
2024-08-16 13:01:42 +0530

    chore: Add Tailwind CSS configuration and global styles

---
+++ src/components/Sidebar/SidebarMain.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+function SidebarMain() {
+  return (
+    <div className ='h-screen w-0 lg:w-[15rem] bg-red-500 '>
+        
+    </div>
+  )
+}
+
+export default SidebarMain
